06c201c71e2302c3d0d835bf6bd02104124aef67
Kushal Vala <kushalvala20@gmail.com>
2021-04-02 12:31:31 +0530

    Enter

--- FastAPI-Keras/app/main.py
+++ FastAPI-Keras/app/main.py
@@ -34,6 +34,5 @@ def predict_review(data: Reviews):
         'prediction': prediction.tolist()[0][0]
     }
 
-
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8010)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
